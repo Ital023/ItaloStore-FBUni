823f26b91c5848b525ab0ca6df9f5e5169c02045
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2024-05-19 14:36:38 -0300

    feat: contador all

--- assets/js/contador.js
+++ assets/js/contador.js
@@ -1,32 +1,71 @@
+
 setTimeout(() => {
-    const contadorDiminuir = document.querySelector(".contador-diminuir");
-    let contadorValue = document.querySelector(".contador-value");
-    const contadorAumentar = document.querySelector(".contador-aumentar");
+    let contadoresValues = document.querySelectorAll(".contador-value");
 
-    let contadorValueNumerico =  parseInt(contadorValue.textContent);
-    console.log(contadorValueNumerico);
 
-    verificarIsOneNumber(contadorValueNumerico, contadorDiminuir);
-    
+    contadoresValues.forEach(contador => {
+        const contadorDiminuir = (contador.parentNode).querySelector(".contador-diminuir");
+        const contadorAumentar = (contador.parentNode).querySelector(".contador-aumentar");
+        let contadorValueNumerico =  parseInt(contador.textContent);
 
-    contadorDiminuir.addEventListener("click", () => {
-        if (contadorValueNumerico > 1) {
-            contadorValueNumerico--;
-            contadorValue.textContent = contadorValueNumerico;
-            verificarIsOneNumber(contadorValueNumerico,contadorDiminuir);
+        const idContador = contador.parentNode.parentNode.parentNode.parentNode.dataset.id;
 
-        }
+         let item = JSON.parse(localStorage.getItem(`produto_${idContador}`));
+         let quantidade = item ? item.quantity : 1;
+
+         contador.textContent = quantidade;
+
+        verificarIsOneNumber(quantidade, contadorDiminuir);
+
+        contadorDiminuir.addEventListener("click", () => {
+                 if (quantidade > 1) {
+                     quantidade--;
+                     contador.textContent = quantidade;
+                     item.quantity = quantidade;
+                     localStorage.setItem(`produto_${idContador}`, JSON.stringify(item));
+                     verificarIsOneNumber(quantidade,contadorDiminuir);
+        
+                 }
+             });
+
+        contadorAumentar.addEventListener("click", () => {
+                     quantidade++;
+                     contador.textContent = quantidade;
+                     item.quantity = quantidade;
+                     localStorage.setItem(`produto_${idContador}`, JSON.stringify(item));
+                     verificarIsOneNumber(quantidade,contadorDiminuir);
+                 });
+
     });
 
-    
-    contadorAumentar.addEventListener("click", () => {
-        if (contadorValueNumerico >= 1) {
-            contadorValueNumerico++;
-            contadorValue.textContent = contadorValueNumerico;
-            verificarIsOneNumber(contadorValueNumerico,contadorDiminuir);
 
-        }
-    })
+
+
+
+    // let contadorValueNumerico =  parseInt(contadorValue.textContent);
+    // console.log(contadorValueNumerico);
+
+    // verificarIsOneNumber(contadorValueNumerico, contadorDiminuir);
+    
+
+    // contadorDiminuir.addEventListener("click", () => {
+    //     if (contadorValueNumerico > 1) {
+    //         contadorValueNumerico--;
+    //         contadorValue.textContent = contadorValueNumerico;
+    //         verificarIsOneNumber(contadorValueNumerico,contadorDiminuir);
+
+    //     }
+    // });
+
+    
+    // contadorAumentar.addEventListener("click", () => {
+    //     if (contadorValueNumerico >= 1) {
+    //         contadorValueNumerico++;
+    //         contadorValue.textContent = contadorValueNumerico;
+    //         verificarIsOneNumber(contadorValueNumerico,contadorDiminuir);
+
+    //     }
+    // })
 
 
 }, 100);
